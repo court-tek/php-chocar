fb00f5c65087eff24cc2ef5e96605e0537a6ecca
court-tek <dishoughng@gmail.com>
2023-12-03 23:51:12 -0330

    added some variables

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
+<?php
+    $name = "Court"; //string
+    $age = 40; // Int
+    $has_kids = false; // Boolean
+    $cash_on_hand = 5.90; // Float
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="main.css">
     <title>Document</title>
 </head>
 <body>
-    <?php
-        echo "hola mundo";
-    ?>
+    <h1><?php echo $name; ?></h1>
 </body>
 </html>
